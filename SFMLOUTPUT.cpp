5748c5174eb97ba786f94eb257479decabcd4221
rey <114239691+indigowitchh@users.noreply.github.com>
2023-03-15 07:51:09 -0600

    Create SFMLOUTPUT.cpp

---
+++ SFMLOUTPUT.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	char Firstinitials;
+	cout << "What is your first initial?" << endl;
+	cin >> Firstinitials;
+	cout << "Your first initial is: " << Firstinitials << endl;
+
+	cout << endl;
+
+	char Secondinitials;
+	cout << "What is your second initial?" << endl;
+	cin >> Secondinitials;
+	cout << "Your second intital is: " << Secondinitials << endl;
+
+	cout << endl;
+	
+	cout << "So your initials are: " << Firstinitials << Secondinitials << endl;
+
+	cout << endl;
+
+	int money;
+	cout << "How much money you got?" << endl;
+	cin >> money;
+	cout << "You have $: " << money << endl;
+
+	cout << endl;
+
+	int ID;
+	cout << "What is your student ID?" << endl;
+	cin >> ID;
+	cout << "Your Student ID is: " << ID << endl;
+}
